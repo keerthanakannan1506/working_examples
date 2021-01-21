5c92a25e3c980284ae706a81541e0aa445b59802
keerthanakannan1506 <keerthanakarthik1506@gmail.com>
2021-01-21 18:23:22 +0530

    modification feilds

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,28 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-   clickMessage = '';
-     values = '';
-
+export class AppComponent {
+  clickMessage = "";
+  values = "";
+  value = "";
 
   onClickMe() {
-    this.clickMessage = 'keekee!';
+    this.clickMessage = "keekee!";
   }
 
-  onKey(event: any)
-   { // without type info
+  onKey(event: any | KeyboardEvent) {
+    // without type info
 
-   console.log(event);
-  
-    this.values += event.target.value + ' $ ';
+    console.log(event);
+
+    this.values += event.target.value + " | ";
+    this.values += (event.target as HTMLInputElement).value + " | ";
+  }
+  onEnter(value: string) {
+    this.value = value;
   }
 }
